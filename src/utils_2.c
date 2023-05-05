9b661b3f46ee27c01940cd82fb93bcf2ea746ffe
Guillaume Rodriguez <gurodrig@z1r6p1.42lyon.fr>
2023-05-05 08:20:23 +0200

    norme

--- src/utils_2.c
+++ src/utils_2.c
@@ -1,17 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gurodrig <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 08:12:06 by gurodrig          #+#    #+#             */
+/*   Updated: 2023/05/05 08:12:32 by gurodrig         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 #include "fdf.h"
 
-static float absf(float x)
+static float	absf(float x)
 {
 	if (x < 0)
 		return (-x);
 	return (x);
 }
 
-float max_size_map(t_fdf *map)
+float	max_size_map(t_fdf *map)
 {
 	unsigned int	i;
 	unsigned int	j;
-	float		max;
+	float			max;
 
 	i = 0;
 	max = (float)map->dimx;
